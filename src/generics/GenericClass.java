e6a246c63bb1928f83ff616b5c69ec5338a2425a
Timur Saglam <uleay@student.kit.edu>
2017-01-07 14:17:56 +0100

    EDIT: Generic class.

--- src/generics/GenericClass.java
+++ src/generics/GenericClass.java
@@ -1,8 +1,11 @@
 package generics;
 
+import classifierPackage.SomeClass;
+import classifierPackage.SomeInterface;
+
 /**
  * This is a class for testing purposes of the Ecore metamodel extraction.
  */
-public class GenericClass1<T> {
+public class GenericClass<T, S extends SomeClass, R extends SomeClass & SomeInterface> {
 
 }
