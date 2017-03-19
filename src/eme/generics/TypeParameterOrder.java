3f84758e5e510964284f3249d3ec05e1b635e0b8
Timur Saglam <uleay@student.kit.edu>
2017-03-19 20:03:05 +0100

    EDIT: Changed order class to make the extracted metamodel valid.

--- src/eme/generics/TypeParameterOrder.java
+++ src/eme/generics/TypeParameterOrder.java
@@ -1,11 +1,13 @@
 package eme.generics;
 
+import java.util.Date;
+
 /**
  * This is a class for testing purposes of the Ecore metamodel extraction.
  */
 public class TypeParameterOrder<A extends C, B, C extends B> {
 
-    public <X extends Z, Y, Z extends Y> X genericMethod(Y myY) {
+    public <X extends Y, Y extends Z, Z extends Date> X genericMethod(Y myY) {
         return null;
     }
 }
