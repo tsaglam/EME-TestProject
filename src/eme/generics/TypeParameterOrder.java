97b3594833757e671733fa12fe2a17c667e9c218
Timur Saglam <uleay@student.kit.edu>
2017-04-04 17:39:52 +0200

    EDIT: Added disclaimer.

--- src/eme/generics/TypeParameterOrder.java
+++ src/eme/generics/TypeParameterOrder.java
@@ -3,7 +3,11 @@ package eme.generics;
 import java.util.Date;
 
 /**
- * This is a class for testing purposes of the Ecore metamodel extraction.
+ * IMPORTANT: EMF does not support a random order in a type parameter hierarchy.
+ * This class tests a feature that is not supported by EMF. This is a class for
+ * testing purposes of the Ecore metamodel extraction but will produce a
+ * metamodel that will lead to the indication of errors. The Java language and
+ * the EcoreMetamodelExtraction both support this feature.
  */
 public class TypeParameterOrder<A extends C, B, C extends B> {
 
