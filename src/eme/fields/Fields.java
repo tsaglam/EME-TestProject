988820b338fa2ebcad5a7bf5ce3db75c8fdd637f
Timur Saglam <uleay@student.kit.edu>
2017-04-05 11:13:24 +0200

    EDIT: Updated class.

--- src/eme/fields/Fields.java
+++ src/eme/fields/Fields.java
@@ -1,5 +1,6 @@
 package eme.fields;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,19 +13,32 @@ import eme.utility.CustomGenericClass;
 /**
  * This is a class for testing purposes of the Ecore metamodel extraction.
  */
-public class Attributes {
-    public Set<Map<Object, List<String>>> bigBadSet;
-    public char[][] charArray2D;
-    public CustomGenericClass<String, SomeClass> complexReference;
-    public int[] intArray;
-    public Integer intObject;
-    public List<Exchanger<String>> listOfExternal;
-    public SomeClass someClass;
-    public List<SomeClass> someClassList;
-    public SomeInterface someInterface;
-    public List<String> someList;
-    public Map<Integer, String> someMap;
-    public String someStringWithAVeryLongNameWhichIsAbsolutlyRidiculous;
-    public Exchanger<String> someWeirdExternalDataType;
-    public String[][][] stringArray3D;
+@SuppressWarnings("unused")
+public class Fields {
+    // basic:
+    private int number;
+    private String text;
+    private Integer intObject;
+    private char someCharacterWithAVeryLongNameWhichIsAbsolutlyRidiculous;
+
+    // array:
+    private int[] intArray;
+    private char[][] charArray2D;
+    private String[][][] stringArray3D;
+    private Date[] dateArray;
+
+    // with generic arguments:
+    private List<String> someList;
+    private Map<Integer, String> someMap;
+    private List<SomeClass> someClassList;
+    private Set<Map<Object, List<String>>> bigBadSet;
+
+    // external types:
+    private Exchanger<String> someWeirdExternalDataType;
+    private List<Exchanger<String>> listOfExternal;
+
+    // References
+    private SomeClass someClass;
+    private SomeInterface someInterface;
+    private CustomGenericClass<String, SomeClass> complexReference;
 }
